50f44529fe4362fcd140b459a6d937c709273b03
ac_airbear <acolby@live.com>
2014-10-24 11:40:42 -0700

    added trapoziod rule

--- public/scripts/controllers/HomeCtrl.js
+++ public/scripts/controllers/HomeCtrl.js
@@ -135,16 +135,14 @@ angular.module('shouldImine')
 						draggableY: true,
 						draggableX: false,
 						color: '#32b69e'
-					},
-					/*
-					{
-						name: 'calculated regression',
-						data: newRegression,
-						draggableY: false,
-						draggableX: false,
-						color: '#328bb5'
-					}	
-					*/
+					}/////,
+					/////{
+					/////	name: 'calculated regression',
+					/////	data: newRegression,
+					/////	draggableY: false,
+					/////	draggableX: false,
+					/////	color: '#328bb5'
+					/////}	
 				]
 			});
 
@@ -161,42 +159,26 @@ angular.module('shouldImine')
 						chart.series[1].yData[i]
 					]);
 				}
-				
-				var theirPredictionRegression = regression('polynomial', theirPerdictionPoints, 3);
 
-				var xValues = [];
-				var yValues = [];
-				for(i = 0; i < theirPerdictionPoints.length; i++){
-					xValues.push(theirPerdictionPoints[i][0]);
-					yValues.push(theirPerdictionPoints[i][1]);
-				}
+				var hashRateTrapazoidAproximation = calculateTrapazoidAproximationFunction(theirPerdictionPoints);
 
-				var times = numeric.linspace(xValues[0],xValues[4],730);
-				var hashRates = numeric.spline(xValues,yValues).at(times);
-
-				// calculate coins mined
-				var totalCoinsMined = 0;
-				for(i = 0; i < hashRates.length; i++){
-					var coinsMindedThatDay = $scope.yourHashRate/(Math.max(hashRates[i], 0)+$scope.yourHashRate)*coinsMindInOneDay/1000;
-					totalCoinsMined += coinsMindedThatDay;
-				}
-				// plot the new regressiong
-				/*
+				var numberOfSecondsMiliSecondsInOneDay = 24*60*60*1000;
+				var timePointer = theirPerdictionPoints[0][0] + numberOfSecondsMiliSecondsInOneDay;
+				var coinsMined = 0;
 				newRegression = [];
-
-				for (i = 0; i < times.length; i++) {
-					time = now+miliSecondsInSixMonths*i/6;
+				while(timePointer < theirPerdictionPoints[4][0]){
+					var coinsMindedThatDay =  $scope.yourHashRate/hashRateTrapazoidAproximation(timePointer)*coinsMindInOneDay/1000;
+					coinsMined = coinsMined + coinsMindedThatDay;
+					timePointer += numberOfSecondsMiliSecondsInOneDay;
 					newRegression.push({
-						'x': times[i],
-						'y': hashRates[i]
+						'x': timePointer,
+						'y': hashRateTrapazoidAproximation(timePointer)
 					});
 				}
-
-				chart.series[1].setData(newRegression,true);
-				*/
-
+				////// chart.series[2].setData(newRegression,true);
+				
 				$scope.totalCosts = $scope.userUpfrontCost + $scope.userMonthlyCost*24;
-				$scope.coinsMined = totalCoinsMined;
+				$scope.coinsMined = coinsMined;
 
 				// data to push to firebase
 				anonomousAnalyticsMyRef.child('claculations').push({
@@ -207,6 +189,42 @@ angular.module('shouldImine')
 					'hashRate': $scope.yourHashRate
 				});
 			};
+
+			function calculateTrapazoidAproximationFunction(p){ // input is the prediction point is form [[x0, y0], [x1, y1]... [xN, yN]]
+
+				// two point form y = (y2 - y1)/(x2 - x1)(x - x1) + y1
+				var curve1 = function(time){
+					return ((p[1][1] - p[0][1])/(p[1][0] - p[0][0]))*(time - p[0][0]) +  p[0][1];
+				};
+				var curve2 = function(time){
+					return ((p[2][1] - p[1][1])/(p[2][0] - p[1][0]))*(time - p[1][0]) +  p[1][1];
+				};
+				var curve3 = function(time){
+					return ((p[3][1] - p[2][1])/(p[3][0] - p[2][0]))*(time - p[2][0]) +  p[2][1];
+				};
+				var curve4 = function(time){
+					return ((p[4][1] - p[3][1])/(p[4][0] - p[3][0]))*(time - p[3][0]) +  p[3][1];
+				};
+
+				return function(time){
+					if(time < p[0][0]){
+						return 0;
+					}else if(time < p[1][0]){
+						// use curve 1
+						return curve1(time);
+					}else if(time < p[2][0]){
+						// use curve 2
+						return curve2(time);
+					}else if(time < p[3][0]){
+						// use curve 3
+						return curve3(time);
+					}else{
+						// use curve 4
+						return curve4(time);
+					}
+					return curve1(time);
+				};
+			}
 		}
 	}
 ]);
